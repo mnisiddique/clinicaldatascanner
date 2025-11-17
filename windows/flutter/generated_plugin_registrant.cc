6695ea62fd4f7faa2ff515551281961d2a36f6bb
mni <mnisiddique30@gmail.com>
2025-11-17 22:46:13 +0600

    dep: removes cunning doc scanner and adds flutter doc scanner

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <permission_handler_windows/permission_handler_windows_plugin.h>
+#include <flutter_doc_scanner/flutter_doc_scanner_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  PermissionHandlerWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
+  FlutterDocScannerPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterDocScannerPluginCApi"));
 }
